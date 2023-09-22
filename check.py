0948590e7f9ae48cc20eed7fbe163e35a4c8d7de
dabao1955 <Admin@qw5d.onmicrosoft.com>
2023-09-22 17:42:21 +0800

    Fri Sep 22 17:42:21 CST 2023

--- check.py
+++ check.py
@@ -3,18 +3,20 @@ import subprocess
 import sys
 from datetime import datetime
 
-try:
-    print("# KernelSU repo status")
-    print()
-    print(f"Updated on {datetime.now().strftime('%Y-%m-%dT%H:%M:%S')}")
-    print()
-    print(" | repo url | repo status |")
-    print(" | -------- | -------- | ")
-    os.system('sleep 1s')
-    subprocess.call(['python', 'bin/geturl.py'])
-    os.system('sleep 1s')
-    os.system('bash bin/checkurl | grep 301')
-    os.system('bash bin/checkurl | grep 404')
+with open("README.md", "w+", encoding="utf-8") as f:
+    sys.stdout = f
 
-except:
-    sys.exit()
+    try:
+        print("# KernelSU repo status")
+        print()
+        print(f"Updated on {datetime.now().strftime('%Y-%m-%dT%H:%M:%S')}")
+        print()
+        print(" | repo url | repo status |")
+        print(" | -------- | -------- | ")
+        subprocess.call(['python', 'bin/geturl.py'])
+        os.system('bash bin/checkurl | grep 301')
+        os.system('bash bin/checkurl | grep 404')
+
+    finally:
+        sys.stdout = sys.__stdout__
+
