3eb2ca555421ee9de1ac922b8958f7c195e23123
dabao1955 <Admin@qw5d.onmicrosoft.com>
2023-09-22 16:41:59 +0800

    revert laet commit

--- check.py
+++ check.py
@@ -4,15 +4,16 @@ import sys
 from datetime import datetime
 
 try:
-    subprocess.call(['python', 'bin/geturl.py'])
-    os.system('bash bin/checkurl | grep 301')
-    os.system('bash bin/checkurl | grep 404')
     print("# KernelSU repo status")
     print()
     print(f"Updated on {datetime.now().strftime('%Y-%m-%dT%H:%M:%S')}")
     print()
     print("| repo url | repo status |")
     print("| -------- | -------- | ")
+    os.system('sleep 1s')
+    subprocess.call(['python', 'bin/geturl.py'])
+    os.system('bash bin/checkurl | grep 301')
+    os.system('bash bin/checkurl | grep 404')
 
 except:
     sys.exit()
