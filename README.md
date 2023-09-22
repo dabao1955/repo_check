f87dabeee67d8475bb284265863b92b01a6ebd5e
dabao1955 <Admin@qw5d.onmicrosoft.com>
2023-09-22 16:28:12 +0800

    Fri Sep 22 16:28:12 CST 2023

--- README.md
+++
@@ -1,8 +0,0 @@
-| https://github.com/Sanju0910/android_kernel_oneplus_avicii |  HTTP/2 301  |
-| https://github.com/Vincent4440/noxious_kernel_xiaomi_sm8250 |  HTTP/2 404  |
-# KernelSU repo status
-
-Updated on $(date '+%Y-%m-%dT%H:%M:%S')
-
-| repo url | repo status |
-
--- check.py
+++ check.py
@@ -1,10 +1,12 @@
 import os
 import subprocess
 import sys
+from datetime import datetime
+
 try:
     print("# KernelSU repo status")
     print()
-    print("Updated on $(date '+%Y-%m-%dT%H:%M:%S')")
+    print(f"Updated on {datetime.now().strftime('%Y-%m-%dT%H:%M:%S')}")
     print()
     print("| repo url | repo status |")
     subprocess.call(['python', 'bin/geturl.py'])
