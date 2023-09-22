ef49d1bd07998550695331f9da7b7f76345d9263
dabao1955 <Admin@qw5d.onmicrosoft.com>
2023-09-22 16:24:54 +0800

    Fri Sep 22 16:24:54 CST 2023

--- README.md
+++
@@ -1,3 +0,0 @@
-| https://github.com/Sanju0910/android_kernel_oneplus_avicii |  HTTP/2 301  |
-| https://github.com/Vincent4440/noxious_kernel_xiaomi_sm8250 |  HTTP/2 404  |
-
--- check.py
+++ check.py
@@ -2,6 +2,11 @@ import os
 import subprocess
 import sys
 try:
+    print("# KernelSU repo status")
+    print()
+    print("Updated on $(date '+%Y-%m-%dT%H:%M:%S')")
+    print()
+    print("| repo url | repo status |")
     subprocess.call(['python', 'bin/geturl.py'])
     os.system('bash bin/checkurl | grep 301')
     print()
